6c1c7f396b5565480bd33801650b7ae8672a5eb2
andresvcc <andres.caballero@etu.unige.ch>
2020-05-18 09:08:30 +0200

    hjk

--- view/views/test/test2.js
+++ view/views/test/test2.js
@@ -1,28 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import citiesList from '../Home/database/countries';
 
 export default function test2(props) {
-  const iii3 = () => {
-    const zzu = [];
-
-    const uu = citiesList.forEach((value) => {
-      if (value.lat && value.lng) {
-        const pos = [JSON.parse(value.lat), JSON.parse(value.lng)];
-        zzu.push({
-          id: value.id,
-          name: value.country,
-          pos
-        });
-      } else {
-        zzu.push(value);
-      }
-    });
-
-    // console.log(zzu);
-    console.log(JSON.stringify(zzu));
-  };
-
-  const ooo = iii3();
   return (
     <div>
       <h1>TEST3</h1>
